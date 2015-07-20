860291a990bbfe355597a08581c5f3e33a95ef1f
Beth Gilomen <gilomen2@gmail.com>
2015-07-19 22:12:15 -0500

    Assignment 32 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ require 'faker'
 # Create Posts
 50.times do
   Post.create!(
-    title:   Faker::Lorem.sentence,
-    body:    Faker::Lorem.paragraph
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph
   )
 end
 posts = Post.all
@@ -12,8 +12,27 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
-    post:   posts.sample,
-    body:   Faker::Lorem.paragraph
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+# Create Unique Post
+unique_title = "This is my unique title"
+unique_body = "This is my unique body."
+if !Post.exists?({:title => unique_title, :body => unique_body})
+  Post.create!(
+    title: unique_title,
+    body:  unique_body
+  )
+end
+
+# Create Unique Comment
+unique_comment_body = "This is my unique comment body."
+if !Comment.exists?({:body => unique_comment_body})
+  Comment.create!(
+    post: posts.sample,
+    body: unique_comment_body
   )
 end
 
