b370b7e46ca162ca5ad445f7dfc4f1dac7a65175
Beth Gilomen <gilomen2@gmail.com>
2015-07-19 20:10:25 -0500

    fixed spelling error in question migration

--- db/migrate/20150720010508_create_questions.rb
+++ db/migrate/20150720010508_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :title
       t.text :body
-      t.boolean :resolves
+      t.boolean :resolved
 
       t.timestamps null: false
     end
