8906d96297909dc7e51c19caa05a9c2d17bd1d11
Beth Gilomen <gilomen2@gmail.com>
2015-07-20 20:55:17 -0500

    Assignment 33 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719233734) do
+ActiveRecord::Schema.define(version: 20150721010959) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
