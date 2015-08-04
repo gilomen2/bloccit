dbab0c0c94e10770a08079efb933299a81b7b3a4
Beth Gilomen <gilomen2@gmail.com>
2015-08-04 00:07:23 -0500

    Assignment 40: post_id not being saved on summary

---
+++ app/controllers/summaries_controller.rb
@@ -0,0 +1,45 @@
+class SummariesController < ApplicationController
+  def show
+    @summary = Summary.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+  end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    @summary = Summary.new
+  end
+
+  def create
+    @summary = Summary.new(params.require(:summary).permit(:body))
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    if @summary.save
+      flash[:notice] = "Summary was saved."
+      redirect_to [@topic, @post, @summary]
+    else
+      flash[:error] = "There was an error saving the summary. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @summary = Summary.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+  end
+
+  def update
+    @summary = Summary.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    if @summary.update_attributes(params.require(:summary).permit(:body))
+      flash[:notice] = "Summary was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the summary. Please try again."
+      render :edit
+    end
+  end
+end
