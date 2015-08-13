9b4aad7b7b3ad894b929b57149fce52bdfb4c57a
Beth Gilomen <gilomen2@gmail.com>
2015-08-12 19:12:00 -0500

    Assignment 42 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#  topic_id   :integer
 #
 
 class Post < ActiveRecord::Base
