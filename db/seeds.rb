483bc7d9db9bab5f86fab90d9a708c4d262882cf
Beth Gilomen <gilomen2@gmail.com>
2015-08-15 15:55:05 -0500

    Assigment 45 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ end
 users = User.all
 
 # Create Topics
-15.times do
+100.times do
   Topic.create!(
     name:         Faker::Lorem.sentence,
     description:  Faker::Lorem.paragraph
@@ -24,13 +24,23 @@ topics = Topic.all
 
 
 # Create Posts
-50.times do
+550.times do
   Post.create!(
     user:    users.sample,
     topic:   topics.sample,
     title:   Faker::Lorem.sentence,
     body:    Faker::Lorem.paragraph
   )
+end
+
+
+300.times do
+  Post.create!(
+    user:   users.sample,
+    topic_id:  51,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+)
 end
 posts = Post.all
 
