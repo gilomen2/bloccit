3c0b9919fc5f36d7ad388a722ce7adfea4f70aef
Beth Gilomen <gilomen2@gmail.com>
2015-08-16 00:40:53 -0500

    Can't render form and comments at the same time

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = @post
+    if @comment.save
+        flash[:notice] = "Comment was saved."
+        redirect_to [@topic, @post]
+      else
+        flash[:error] = "There was an error saving the comment. Please try again."
+        render :new
+    end
+  end
+
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
+end
