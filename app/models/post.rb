fb7dde8c31d052bc5d571a209cc5796248ae3c86
Beth Gilomen <gilomen2@gmail.com>
2015-09-01 21:21:00 -0500

    Checkpoint 50: Mocking with RSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -41,14 +41,10 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   validates_associated :comments
-
-  after_create :create_vote
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
