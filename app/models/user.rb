85862e8331d25c884767b1caec280e41796ac749
Beth Gilomen <gilomen2@gmail.com>
2015-09-13 13:09:31 -0500

    Checkpoint 55: Popular Posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,4 +51,15 @@ class User < ActiveRecord::Base
     votes.where(post_id: post.id).first
   end
 
+  def self.top_rated
+    self.select('users.*') # Select all attributes of the user
+        .select('COUNT(DISTINCT comments.id) AS comments_count') #Count the comments made by a user
+        .select('COUNT(DISTINCT posts.id) AS posts_count') #Count the posts made by a user
+        .select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id)') #Add the comment count to the post count and label the sum as "rank"
+        .joins(:posts) #Ties the posts table to the users table, via the user_id
+        .joins(:comments) #Ties the comments table to the users table, via the user_id
+        .group('users.id') #Instructs the database to group results so that each user will be returned ina  distinct row
+        .order('rank DESC') #Instructs the database to order the results in descending order, by the rank that we created in this query. (rank = comment count + post count)
+  end
+
 end
