837f88f5e56bc4794342477e0360a08f01960a34
Beth Gilomen <gilomen2@gmail.com>
2015-09-13 21:11:37 -0500

    Checkpoint 58: Creating with AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,20 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
-    @comment = @post.comments.new(comment_params)
-    @comment.user_id = current_user.id
+
+    @comments = @post.comments
+
+    @comment = Comment.new( comment_params )
+    @comment.user = current_user
+    @comment.post = @post
+    @new_comment = Comment.new
+
     authorize @comment
+
     if @comment.save
       flash[:notice] = "Comment was saved."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@topic, @post]
     end
   end
 
